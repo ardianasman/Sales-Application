9406189167c73a29b75456f52fd2a7c8bce570bc
RichardoJ <79075790+RichardoJ@users.noreply.github.com>
2021-10-29 11:41:48 +0700

    query showimageupload php fix

--- services/get_all_images.php
+++ services/get_all_images.php
@@ -9,7 +9,12 @@ header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     $id_sales = $_SESSION['id'];
-    $sql = "SELECT a.id_aktivitas,s.nama,a.jadwal_kunjungan,a.foto_kunjungan FROM aktivitas_sales a JOIN sales s ON a.id_sales = s.id_sales WHERE s.id_sales = ? AND a.status_kunjungan = 1";
+    $sql = "SELECT a.id_aktivitas,c.nama AS Nama_Cust ,s.nama,a.jadwal_kunjungan,a.foto_kunjungan
+    FROM aktivitas_sales a 
+    JOIN sales s ON a.id_sales = s.id_sales
+    JOIN `order` ON a.id_order = `order`.`id_order`
+    JOIN customer c ON `order`.`id_customer` = c.id_customer
+    WHERE a.id_sales = ? AND a.status_kunjungan = 1";
     $stmt = $pdo->prepare($sql);
     $stmt->execute([$id_sales]);
 
