8865e776a7f894ba95710a9ed58a4ec993bf1c99
RichardoJ <79075790+RichardoJ@users.noreply.github.com>
2021-11-28 11:37:17 +0700

    tambahan waktu di fitur tracking

--- services/check_position.php
+++ services/check_position.php
@@ -6,6 +6,10 @@ header("Content-Type: application/json");
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
+    date_default_timezone_set('Asia/Jakarta');
+
+    $tanggal = date('Y-m-d h:i:sa');
+
     $result = array(
         "status" => 1,
         "error" => "",
@@ -16,9 +20,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $plg = $_POST['posisi_lg'];
     $simpanid = $_SESSION['id'];
 
-    $sql_1 = "UPDATE sales SET track_lat = ?, track_lng = ? WHERE id_sales = ?";
+    $sql_1 = "UPDATE sales SET track_lat = ?, track_lng = ?, track_time = ? WHERE id_sales = ?";
     $test_2 = $pdo->prepare($sql_1);
-    $test_2->execute([$plt,$plg,$simpanid]);
+    $test_2->execute([$plt,$plg,$tanggal,$simpanid]);
 
     echo json_encode($result);
 } else {
